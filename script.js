eca4c155f0c59b18d3419a0f8ebdc4c4b29184ff
matheusilva05 <matheusalveshen06@gmail.com>
2026-03-25 13:32:21 -0300

    primeira versão do projeto

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("calcular").addEventListener("click", function(){
+    let salario = Number(document.getElementById("salario").value);
+    let produto = Number(document.getElementById("Produto").value);
+
+    let salario_dia = salario / 30;
+    let salario_hora = salario_dia / 8;
+
+    let horas = produto / salario_hora;
+    let dias = horas / 8;
+
+    document.getElementById("resultado").innerText = 
+    `Você precisa trabalhar ${Math.ceil(dias)} dias para comprar esse produto`;
+})
+
+
