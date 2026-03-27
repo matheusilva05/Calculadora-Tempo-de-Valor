acff59e30602f0d14caeb60e1dad52c4d86eac58
matheusilva05 <matheusalveshen06@gmail.com>
2026-03-26 21:01:00 -0300

    Alterando design, ajustes e adicionando img

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.getElementById("calcular").addEventListener("click", function(){
     let salario = Number(document.getElementById("salario").value);
     let produto = Number(document.getElementById("Produto").value);
+    let product = document.getElementById("product").value;
+
 
     let salario_dia = salario / 30;
     let salario_hora = salario_dia / 8;
@@ -9,7 +11,7 @@ document.getElementById("calcular").addEventListener("click", function(){
     let dias = horas / 8;
 
     document.getElementById("resultado").innerText = 
-    `Você precisa trabalhar ${Math.ceil(dias)} dias para comprar esse produto`;
+    `Você precisa trabalhar ${Math.ceil(dias)} dias para comprar esse ${(product)}`;
 })
 
 
